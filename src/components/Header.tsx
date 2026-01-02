c5c57b3c3a42ca68fbd6b456c4c25b18433f7e17
kevin finnissee <kevinduncan1993@gmail.com>
2026-01-02 01:20:43 -0500

    Update brand name to ContentIQ in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
             <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-primary-600">
               <Sparkles className="h-5 w-5 text-white" />
             </div>
-            <span className="text-xl font-bold text-gray-900">RepurposeFlow</span>
+            <span className="text-xl font-bold text-gray-900">ContentIQ</span>
           </Link>
 
           {/* Navigation */}
